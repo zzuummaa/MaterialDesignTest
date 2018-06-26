63af15d97357049d6d4ef831b054f2e74fd8fc25
Stephan <stepannk@mail.ru>
2018-06-27 00:21:02 +0300

    Material product adapter & view

--- app/src/main/java/ru/zuma/materialdesigntest/MainActivity.kt
+++ app/src/main/java/ru/zuma/materialdesigntest/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.zuma.materialdesigntest.db.PREF_COOKIES
 import ru.zuma.materialdesigntest.rest.Product
@@ -15,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         val REQ_AUTH = 1
     }
 
-    val productList = ArrayList<Product>()
-    lateinit var productAdapter: ProductAdapter
+    private val productList = ArrayList<Product>()
+    private lateinit var productAdapter: ProductAdapter
+
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +32,28 @@ class MainActivity : AppCompatActivity() {
         if (cookie.equals("")) {
             val intent = Intent(this, ChooseAuthActivity::class.java)
             startActivityForResult(intent, REQ_AUTH)
+        } else {
+            tvCookie.text = "Cookie:" + cookie
         }
 
         productList.add(Product(1234, "Техника", "Смартфон", 12_500f, 3f))
         productList.add(Product(2345, "Игры", "Кукла", 1_300f, 0f))
-        productAdapter = ProductAdapter(this, productList)
+        productAdapter = ProductAdapter(productList)
 
-        lvMain.adapter = productAdapter
+        viewManager = LinearLayoutManager(this)
+
+        //lvMain.adapter = productAdapter
+        rvMain.apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = productAdapter
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/ru/zuma/materialdesigntest/ProductAdapter.kt
+++ app/src/main/java/ru/zuma/materialdesigntest/ProductAdapter.kt
@@ -1,101 +1,47 @@
 package ru.zuma.materialdesigntest
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.util.Log
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import ru.zuma.materialdesigntest.rest.Product
 import android.view.LayoutInflater
 import android.widget.*
 
+class ProductAdapter(private val products: List<Product>) :
+        RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
 
-/**
- * Created by Stephan on 26.06.2018.
- */
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-class ProductAdapter(val context: Context, val products: List<Product>) : BaseAdapter() {
 
-    var lInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): ProductAdapter.ViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.activity_main_product_item, parent, false)
+        // set the view's size, margins, paddings and layout parameters
 
-    // кол-во элементов
-    override fun getCount(): Int {
-        return products.size
+        return ViewHolder(view)
     }
 
-    // элемент по позиции
-    override fun getItem(position: Int): Any {
-        return products[position]
-    }
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
 
-    // id по позиции
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    // пункт списка
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        // используем созданные, но не используемые view
-//        var view: View? = convertView
-//        if (view == null) {
-//            view = lInflater.inflate(R.layout.activity_main_product_item, parent, false)
-//        }
-
-//        val p = getProduct(position)
-//
-//        // заполняем View в пункте списка данными из товаров
-//        (view!!.findViewById(R.id.tvProductID) as TextView).text = "№ ${p.id}"
-//        (view.findViewById<View>(R.id.tvProductCategory) as TextView).text = "${p.category} руб."
-//        (view.findViewById<View>(R.id.tvProductName) as TextView).text = p.name
-//        (view.findViewById<View>(R.id.tvPrice) as TextView).text = "${p.price} руб."
-//
-//        markStars(view.findViewById<View>(R.id.llStars) as LinearLayout, p.stars!!.toInt())
-//
-//        return view
-//    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var view: View? = convertView
-        if (view == null) {
-            view = lInflater.inflate(R.layout.activity_main_product_item, parent, false)
-        }
-
-        val p = getProduct(position)
+        val p = products[position]
+        val view = holder.view
 
         (view!!.findViewById(R.id.tvProductID) as TextView).text = "№ ${p.id}"
         (view.findViewById<View>(R.id.tvProductCategory) as TextView).text = p.category
         (view.findViewById<View>(R.id.tvProductName) as TextView).text = p.name
         (view.findViewById<View>(R.id.tvPrice) as TextView).text = "${p.price} руб."
-
-        val stars = p.stars?.toInt()
-        if (stars != null) {
-            markStars(view.findViewById<View>(R.id.llStars) as LinearLayout, stars)
-        }
-
-        return view!!
     }
 
-    private inline fun markStars(llStars: LinearLayout, stars: Int) {
-
-        if (stars > 5 || stars < 0) {
-            Log.e(javaClass.simpleName, "Invalid stars count: $stars")
-            return
-        }
-
-        for (i in 0..(stars-1)) {
-            llStars.getChildAt(i)?.visibility = View.VISIBLE
-            llStars.getChildAt(i + 5)?.visibility = View.GONE
-        }
-
-        for (i in (stars)..4) {
-            llStars.getChildAt(i)?.visibility = View.GONE
-            llStars.getChildAt(i + 5)?.visibility = View.VISIBLE
-        }
-    }
-
-    // товар по позиции
-    fun getProduct(position: Int): Product {
-        return getItem(position) as Product
-    }
-
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = products.size
 }
